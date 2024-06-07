4f6f9af59f3d2f62c580e86beb5d16d19f8c8456
Diego Loayza <diegoald2004@gmail.com>
2024-06-06 19:56:30 -0500

    .

--- back-end/src/main/java/com/utp/desarrollo/backend/models/Producto.java
+++ back-end/src/main/java/com/utp/desarrollo/backend/models/Producto.java
@@ -1,5 +1,12 @@
 package com.utp.desarrollo.backend.models;
 
 public class Producto {
-
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String marca;
+    private String imagen;
+    private double precioRegular;
+    private double precioDescuento;
+    private double descuento;
 }
