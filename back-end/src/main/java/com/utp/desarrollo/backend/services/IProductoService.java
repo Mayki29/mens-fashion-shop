aa7dbe67955040cbad4dcd9db1f70d45e0581f6a
Mayki <zicmayki@gmail.com>
2024-06-13 14:11:20 -0500

    metodos producto service

--- back-end/src/main/java/com/utp/desarrollo/backend/services/IProductoService.java
+++ back-end/src/main/java/com/utp/desarrollo/backend/services/IProductoService.java
@@ -6,4 +6,8 @@ import com.utp.desarrollo.backend.models.Producto;
 
 public interface IProductoService {
     List<Producto> getAll();
+    Producto findById(Long id);
+    void save(Producto producto);
+    void update(Producto producto);
+    void delete(Long id);
 }
