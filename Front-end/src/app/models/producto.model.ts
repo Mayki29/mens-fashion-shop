62af68525897ecd3302917d777f9cadc2422e438
Mayki <zicmayki@gmail.com>
2024-07-23 22:52:51 -0500

    merge producto model

--- Front-end/src/app/models/producto.model.ts
+++ Front-end/src/app/models/producto.model.ts
@@ -2,13 +2,7 @@
 
 import { Marca } from './marca.model';
 import { Categoria } from './categoria.model';
-<<<<<<< HEAD
-import { Talla } from './talla.model';
 import { Inventario } from './inventario.model';
-
-=======
-import { Inventario } from './inventario.model';
->>>>>>> b74ca873f18c7827bc1c0efb70d048e570455552
 export interface Producto {
   id: number;
   nombre: string;
@@ -21,9 +15,5 @@ export interface Producto {
   descuento: number;
   imagenUrl: string;
   imagenUrlSec: string[];
-<<<<<<< HEAD
-  tallas: Talla[];
-=======
->>>>>>> b74ca873f18c7827bc1c0efb70d048e570455552
   inventario: Inventario[];
 }
